ab7082dd045c58b9e1266e811e1750e7b2f7901a
Coinmaama <coinmaama@gmail.com>
2025-06-17 15:58:13 +0530

    Delete App directory

--- App/handlers.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
-
-def setup_handlers(application):
-    application.add_handler(CommandHandler("start", start_handler))
-
-async def start_handler(update: Update, context: CallbackContext):
-    await update.message.reply_text("Welcome to Real Estate Bot!")
